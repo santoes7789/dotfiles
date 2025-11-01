0303bbf4e2a8fc4829db4b70122ba48c97c9515f
aj <faronsantoso105@gmail.com>
2025-11-01 22:51:49 +1300

    wob, tofi and mako?

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -4,5 +4,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.cmd("set clipboard+=unnamedplus")
+
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
